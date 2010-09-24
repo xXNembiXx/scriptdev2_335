1bbc516c278358aa90c5d865bdb7d888f23b5ae7
Janu <jaed1@gmx.net>
2010-09-25 01:46:43 +0200

    [S58] fixed SQL
    Signed-off-by: Janu <jaed1@gmx.net>

--- Custom/[S20] Tree_Company_mangos.sql
+++ Custom/[S20] Tree_Company_mangos.sql
@@ -7,7 +7,9 @@ DELETE FROM `creature_ai_scripts` WHERE creature_id=17318;
 -- adding event handling for spell event
 
 DELETE FROM `event_scripts` WHERE (`id`='10675');
-INSERT INTO event_scripts () VALUES
+INSERT INTO event_scripts
+	(`id`, `delay`, `command`, `datalong`, `datalong2`, `dataint`, `x`, `y`, `z`, `o`)
+VALUES
    (10675, 0, 10, 17243, 80000, 0, -5029.91, -11291.79, 8.096, 6.27609),
    (10675, 90, 8, 17243, 1, 0, 0, 0, 0, 0),
    (10675, 1, 10, 17318, 80000, 0, -5152.65, -11250.14, 3.6, 2.82662);
